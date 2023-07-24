f4f14bc0b7b1d3dea5a05255104e02a388a60933
NateVoyages <allanromo20@gmail.com>
2023-07-23 18:22:19 -0700

    Add random pokemon a& display name basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Card from './components/Card'
 import './App.css'
 
+
 function App() {
   const[gameState, setGameState] = useState(false);
+  let names = useRef([randomId(50),randomId(100), randomId(150),
+    randomId(200),randomId(250), randomId(300),
+    randomId(350),randomId(400), randomId(450),
+    randomId(450),randomId(450), randomId(500),
+  ])
+  let mapRef = useRef(new Map());
+  function randomId(value) {
+    let increment = Math.floor(value/51);
+    return Math.floor(Math.random()*value) + 1 + increment * 50;
+  }
+  async function fetchSrcData(id) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`, {
+      mode: 'cors'
+    })
+    let pokemon = await response.json();
+    let pokemonSrc = pokemon.sprites.other['official-artwork'].front_default;
+    return pokemonSrc;
+  }
+  async function fetchName(id) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`, {
+      mode: 'cors'
+    })
+    let pokemon = await response.json();
+    let pokemonName = pokemon.name;
+    return pokemonName;
+  }
+  if(mapRef.current.size == 0){
+    names.current.forEach((name)=> mapRef.current.set(name, {src: null, name: null,  selected: false}));
+
+  mapRef.current.forEach((value, key, map)=> {
+    map.set(key, { src:fetchSrcData(key), name: fetchName(key), selected : false});
+
+    });
+  }
+
+
 
   return (
     <>
@@ -11,8 +48,7 @@ function App() {
       <h1>Memory Card</h1>
     </nav>
     <main>
-      <Card setGameState={setGameState}/>
-      <Card setGameState={setGameState}/>
+      <Card setGameState={setGameState} pokemon={mapRef.current.get(names.current[0])} />
       {gameState && <div> YOU LOSE!!!</div>}
 
     </main>
@@ -20,7 +56,8 @@ function App() {
       natevoyages @2023
     </footer>
     </>
-  )
+  );
+
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function Card({setGameState}){
+function Card({setGameState, pokemon}){
     const[selected, setSelected] = useState(false)
-
+    const[src, setSrc] = useState(null);
+    const[name, setName] = useState('null');
+    useEffect(() => {
+        pokemon.src.then( data => setSrc(data));
+        pokemon.name.then( data => setName(data));
+    }, [pokemon.src, pokemon.name])
+        
     function select(){
         if(selected){
         setGameState(true);
         }
         else{
             setSelected(true);
-            console.log('trueeeee');
         }
     }
 
-
-
-    return (<div onClick={select}>Card</div>);
+        return (
+            <div className ="card"
+            onClick={select}>
+                <img src={src} alt="pokemon" height='90px' width='auto' border = '1px solid black'/>
+                <p>{name}</p>
+            </div>);
 }
 
 export default Card;
